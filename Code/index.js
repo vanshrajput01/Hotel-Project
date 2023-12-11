be40a67af42be9965a5652be128b38181965bfaf
vanshrajput01 <rajputvansh009@gmail.com>
2023-12-11 19:20:39 +0530

    some more update in website "Customer section added"

--- Code/index.js
+++ Code/index.js
@@ -52,3 +52,21 @@ if(window.outerWidth > 1024){
     });
   
   }
+
+
+let  swiper01 = new Swiper(".mySwiper01", {
+    spaceBetween: 30,
+    centeredSlides: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    navigation: {
+      nextEl: ".swiper-button-next",
+      prevEl: ".swiper-button-prev",
+    },
+  });
