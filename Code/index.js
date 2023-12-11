269a9bebf23b415bd5e72857a8de61395d3f6c73
vanshrajput01 <rajputvansh009@gmail.com>
2023-12-11 18:07:48 +0530

    some updates in website

---
+++ Code/index.js
@@ -0,0 +1,54 @@
+if(window.outerWidth > 1024){
+    let swiper = new Swiper(".mySwiper", {
+      loop:true,
+      autoplay:true,
+      autoplayTimeout:1000,
+      // autoplayHoverPause : true,
+      // effect : "coverflow",
+      grabCursor : true,
+      centeredSliders : true,
+      slidesPerView : "auto",
+      slidesPerView: 3,
+      spaceBetween: 30,
+      // coverflowEffect : {
+      //   rotate : 0,
+      //   stretch : 0,
+      //   depth : 300,
+      //   modifier : 1,
+      //   sideShadows : false
+  
+      // },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  
+  }
+  else{
+    let swiper = new Swiper(".mySwiper", {
+      loop:true,
+      autoplay:true,
+      autoplayTimeout:1000,
+      // autoplayHoverPause : true,
+      // effect : "coverflow",
+      grabCursor : true,
+      centeredSliders : true,
+      slidesPerView : "auto",
+      slidesPerView: 1,
+      spaceBetween: 30,
+      // coverflowEffect : {
+      //   rotate : 0,
+      //   stretch : 0,
+      //   depth : 300,
+      //   modifier : 1,
+      //   sideShadows : false
+  
+      // },
+      pagination: {
+        el: ".swiper-pagination",
+        clickable: true,
+      },
+    });
+  
+  }
